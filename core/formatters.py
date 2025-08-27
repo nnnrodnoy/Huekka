3c7c288fa475e34404db47cc1015e916ab4e8946
nnnrodnoy <mazalovstepan99@gmail.com>
2025-08-28 00:13:58 +0300

    Update formatters.py

--- core/formatters.py
+++ core/formatters.py
@@ -1,7 +1,7 @@
 # ©️ nnnrodnoy, 2025
 # 💬 @nnnrodnoy
 # This file is part of Huekka
-# 🌐 https://github.com/stepka5/Huekka
+# 🌐 https://github.com/nnnrodnoy/Huekka/
 # You can redistribute it and/or modify it under the terms of the MIT License
 # 🔑 https://opensource.org/licenses/MIT
 import logging

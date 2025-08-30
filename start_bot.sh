00e0337779dace61902b9233aae1d264bf0394ff
nnnrodnoy <mazalovstepan99@gmail.com>
2025-08-30 15:28:17 +0300

    Update start_bot.sh

--- start_bot.sh
+++ start_bot.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
 # ©️ nnnrodnoy, 2025
 # 💬 @nnnrodnoy
 # This file is part of Huekka
 # 🌐 https://github.com/nnnrodnoy/Huekka/
 # You can redistribute it and/or modify it under the terms of the MIT License
 # 🔑 https://opensource.org/licenses/MIT
+
+# Активируем виртуальное окружение
 source Huekka/bin/activate
 
+# Запускаем бота
 python3 main.py

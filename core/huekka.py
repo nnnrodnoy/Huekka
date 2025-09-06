09cea0eccf334038c22f03c99b6f15c5d9e31541
nnnrodnoy <mazalovstepan99@gmail.com>
2025-09-06 15:33:22 +0300

    Update huekka.py

--- core/huekka.py
+++ core/huekka.py
@@ -20,7 +20,7 @@ def get_module_info():
     return {
         "name": "Huekka",
         "description": "Информация о боте Huekka и работа с эмодзи",
-        "developer": "@BotHuekka",
+        "developer": "@B1111otHuekka",
         "version": "1.0.0",
         "commands": [
             {
